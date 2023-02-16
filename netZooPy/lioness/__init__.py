26b0a3dbe595d03ff5f7c7e057e7a62dce4b45c6
violafanfani <viol.fanfani@gmail.com>
2023-02-15 21:53:43 -0500

    adding otter_gpu

--- netZooPy/lioness/__init__.py
+++ netZooPy/lioness/__init__.py
@@ -2,5 +2,6 @@ from __future__ import absolute_import
 
 from .lioness import Lioness
 from .aggro_lioness import AggroLioness
+from .lioness_for_otter import LionessOtter
 #from .analyze_lioness import AnalyzeLioness
 from .lioness_for_puma import LionessPuma
